ce8bb86218930cced9f62a7a610c7391584c03f0
llxjr <43977885+llxjr@users.noreply.github.com>
2023-04-16 16:29:04 +0800

    管理数据2-检索运费价格，创建配送组件

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 import { Injectable } from '@angular/core';
 
@@ -8,7 +9,9 @@ export class CartService {
 
   items: Product[] = [];
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) {}
 
   addToCart(product: Product) {
     this.items.push(product);
@@ -22,4 +25,9 @@ export class CartService {
     this.items = [];
     return this.items;
   }
+
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
+  }
+  
 }
